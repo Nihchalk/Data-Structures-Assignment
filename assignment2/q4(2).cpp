a0ac84e7bf90da0936ab5e7e51764e20e0dc99a3
Nihchalk <nkaur3_be24@thapar.edu>
2025-08-20 15:22:50 +0530

    Update q4(2).cpp

--- assignment2/q4(2).cpp
+++ assignment2/q4(2).cpp
@@ -1,32 +1,15 @@
-#include <iostream>
-using namespace std ;
+#include<iostream>
+using namespace std;
 
-void revstring(char s1[100])
-{
-	int i = 0;
-	while (s1[i] != '\0') {
-		i++;
-	}
-
-	int start = 0, end = i - 1;
-	while (start < end)
-	{
-		char temp = s1[start];
-		s1[start] = s1[end];
-		s1[end] = temp;
-		start++;
-		end--;
-
-	}
-	cout << "Reversed string : " << s1 << endl;
-}
-int main() {
-	char s1[100];
-
-	cout << "Enter string 1:" << endl;
-	cin >> s1;
-
-	revstring(s1);
-
-	return 0;
+int main(){;
+  string s;
+  cout<<"Enter a string : ";
+  getline(cin , s);
+  int n = s.length();
+  for(int i = 0 ; i < n/2; i++){
+    swap(s[i],s[n-i-1]);
+  }
+  cout<<"Reversed string is : "<<endl;
+  cout<<s;
+  return 0;
 }
