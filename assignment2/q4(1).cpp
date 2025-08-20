4ccbce005d24f2fa423d129b0386b505e0778896
Nihchalk <nkaur3_be24@thapar.edu>
2025-08-20 15:23:32 +0530

    Update q4(1).cpp

--- assignment2/q4(1).cpp
+++ assignment2/q4(1).cpp
@@ -1,32 +1,15 @@
-#include <iostream>
-using namespace std ;
+#include<iostream>
+#include<string>
+using namespace std;
 
-// que 4 part 1
-
-int main() {
-	char s1[100], s2[100];
-
-	cout << "Enter string 1:" << endl;
-	cin >> s1;
-
-	cout << "Enter string 2:" << endl;
-	cin >> s2;
-
-	int i = 0;
-	while (s1[i] !='\0') {
-		i++;
-	}
-
-	int j = 0;
-	while (s2[j] != '\0') {
-		s1[i] = s2[j];
-		i++;
-		j++;
-	}
-
-	s1[i] = '\0';
-	cout << endl;
-	cout << "Concatenated string:" << s1 << endl;
-
-	return 0;
+int main(){
+  string s1, s2;
+  cout<<"Enter string 1"<<endl;
+  getline(cin, s1);
+  cout<<"Enter string 2"<<endl;
+  getline(cin , s2);
+  cout<<"Concatenated strings : "<<endl;
+  string result = s1 + " " + s2;
+  cout<<result;
+  return 0;
 }
