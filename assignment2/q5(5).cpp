9a77b2a7f1b236901092eb2e02d5d8c6e3ed1ba8
Nihchalk <nkaur3_be24@thapar.edu>
2025-08-20 20:31:56 +0530

    Create q5(5).cpp

---
+++ assignment2/q5(5).cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Size of array ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter array elements";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int c= 0; 
+
+    for (int i = 0; i < n; i++)
+    {
+        int k = 0; 
+
+        for (int j = 0; j < i; j++)
+        {
+            if (arr[i] == arr[j])
+            {
+                k = 1;
+                break;
+            }
+        }
+
+        if (k== 0) 
+        {
+            c++;
+        }
+    }
+
+    cout << "Total number of distinct elements: " << c << endl;
+
+    return 0;
+}
