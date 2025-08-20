07492e418bfb6a53026174a85caf192737b1d208
Nihchalk <nkaur3_be24@thapar.edu>
2025-08-20 15:22:11 +0530

    Update q4(3).cpp

--- assignment2/q4(3).cpp
+++ assignment2/q4(3).cpp
@@ -1,36 +1,20 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-void deletevowel(char s1[100]) {
-	int i = 0;
-	while (s1[i] != '\0') {
-		i++;
-	}
+int main(){
+  string s;
+  cout<<"Enter a string : ";
+  getline(cin, s);
 
-	int j = 0;
-	while (j < i) 
-	{
-		if (s1[j] == 'a' || s1[j] == 'e' || s1[j] == 'i' || s1[j] == 'o' || s1[j] == 'u' || s1[j] == 'A' || s1[j] == 'E' || s1[j] == 'I' || s1[j] == 'O' || s1[j] == 'U')
-		{
-			for (int k = j; k < i; k++)
-			{
-				s1[k] = s1[k + 1];
-			}
-			i--;
-		}
-		else j++;
-	}
+  int n = s.length();
+  for(int i=0; i<n; i++){
+    if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' || s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U'){
+      s.erase(i,1);
+      n--;
+    }
+  }
 
-
-	cout << "String w/o vowels : " << s1 << endl;
-}
-int main()
-{
-	char s1[100];
-	cout << "Enter string: " << endl;
-	cin >> s1;    
-	 
-	deletevowel(s1);
-
-	return 0;
+  cout<<"String after removing the vowel :\n";
+  cout<<s;
+  return 0;
 }
