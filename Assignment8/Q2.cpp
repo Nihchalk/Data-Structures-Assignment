7dc1b798575e508cc0d1dee6baade87baa1e903a
Nihchalk <nkaur3_be24@thapar.edu>
2025-11-05 21:41:31 +0530

    Update Q2.cpp

--- Assignment8/Q2.cpp
+++ Assignment8/Q2.cpp
@@ -1,126 +1,94 @@
-#include <iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
-struct Node {
+class Node{
+public:
     int data;
-    Node* left;
-    Node* right;
-
-    Node(int val) {
+    Node *left, *right;
+    Node(int val){
         data = val;
-        left = right = nullptr;
+        left = right = NULL;
     }
 };
 
-
-Node* searchRecursive(Node* root, int key) {
-    if (root == nullptr || root->data == key)
-        return root;
-    if (key < root->data)
-        return searchRecursive(root->left, key);
-    else
-        return searchRecursive(root->right, key);
-}
-
-
-Node* searchIterative(Node* root, int key) {
-    while (root != nullptr) {
-        if (key == root->data)
-            return root;
-        else if (key < root->data)
-            root = root->left;
-        else
-            root = root->right;
-    }
-    return nullptr;
-}
-
-
-Node* findMax(Node* root) {
-    if (root == nullptr) return nullptr;
-    while (root->right != nullptr)
-        root = root->right;
+Node* insert(Node* root, int val){
+    if(!root) return new Node(val);
+    if(val < root->data) root->left = insert(root->left, val);
+    else root->right = insert(root->right, val);
     return root;
 }
 
+Node* searchRec(Node* root, int key){
+    if(!root || root->data == key) return root;
+    if(key < root->data) return searchRec(root->left, key);
+    return searchRec(root->right, key);
+}
 
-Node* findMin(Node* root) {
-    if (root == nullptr) return nullptr;
-    while (root->left != nullptr)
-        root = root->left;
+Node* searchNonRec(Node* root, int key){
+    while(root){
+        if(root->data == key) return root;
+        if(key < root->data) root = root->left;
+        else root = root->right;
+    }
+    return NULL;
+}
+
+Node* findMin(Node* root){
+    while(root && root->left) root = root->left;
     return root;
 }
 
-
-Node* inorderSuccessor(Node* root, Node* target) {
-    if (target->right != nullptr)
-        return findMin(target->right);
-
-    Node* successor = nullptr;
-    while (root != nullptr) {
-        if (target->data < root->data) {
-            successor = root;
-            root = root->left;
-        } else if (target->data > root->data) {
-            root = root->right;
-        } else {
-            break;
-        }
-    }
-    return successor;
-}
-
-
-Node* inorderPredecessor(Node* root, Node* target) {
-    if (target->left != nullptr)
-        return findMax(target->left);
-
-    Node* predecessor = nullptr;
-    while (root != nullptr) {
-        if (target->data > root->data) {
-            predecessor = root;
-            root = root->right;
-        } else if (target->data < root->data) {
-            root = root->left;
-        } else {
-            break;
-        }
-    }
-    return predecessor;
-}
-
-
-Node* insert(Node* root, int val) {
-    if (root == nullptr)
-        return new Node(val);
-    if (val < root->data)
-        root->left = insert(root->left, val);
-    else
-        root->right = insert(root->right, val);
+Node* findMax(Node* root){
+    while(root && root->right) root = root->right;
     return root;
 }
 
+Node* inorderSuccessor(Node* root, Node* x){
+    Node* succ = NULL;
+    while(root){
+        if(x->data < root->data){
+            succ = root;
+            root = root->left;
+        } else root = root->right;
+    }
+    return succ;
+}
 
-int main() {
-    Node* root = nullptr;
-    int values[] = {20, 10, 30, 5, 15, 25, 35};
-    for (int val : values)
-        root = insert(root, val);
+Node* inorderPredecessor(Node* root, Node* x){
+    Node* pred = NULL;
+    while(root){
+        if(x->data > root->data){
+            pred = root;
+            root = root->right;
+        } else root = root->left;
+    }
+    return pred;
+}
 
-    int key = 15;
-    Node* foundRec = searchRecursive(root, key);
-    Node* foundIter = searchIterative(root, key);
-    cout << "Recursive Search: " << (foundRec ? "Found" : "Not Found") << endl;
-    cout << "Iterative Search: " << (foundIter ? "Found" : "Not Found") << endl;
+int main(){
+    Node* root = NULL;
+    root = insert(root, 20);
+    insert(root, 10);
+    insert(root, 30);
+    insert(root, 5);
+    insert(root, 15);
+    insert(root, 25);
+    insert(root, 35);
 
-    cout << "Maximum Element: " << findMax(root)->data << endl;
-    cout << "Minimum Element: " << findMin(root)->data << endl;
+    Node* x = searchRec(root, 15);
+    cout<<"Search Recursive: "<<(x ? "Found" : "Not Found")<<"\n";
 
-    Node* target = searchRecursive(root, 20);
-    Node* succ = inorderSuccessor(root, target);
-    Node* pred = inorderPredecessor(root, target);
-    cout << "In-order Successor of 20: " << (succ ? succ->data : -1) << endl;
-    cout << "In-order Predecessor of 20: " << (pred ? pred->data : -1) << endl;
+    x = searchNonRec(root, 25);
+    cout<<"Search Non-Recursive: "<<(x ? "Found" : "Not Found")<<"\n";
+
+    cout<<"Min: "<<findMin(root)->data<<"\n";
+    cout<<"Max: "<<findMax(root)->data<<"\n";
+
+    Node* node = searchRec(root, 25);
+    Node* s = inorderSuccessor(root, node);
+    Node* p = inorderPredecessor(root, node);
+    cout<<"Successor of 25: "<<(s ? to_string(s->data) : "None")<<"\n";
+    cout<<"Predecessor of 25: "<<(p ? to_string(p->data) : "None")<<"\n";
 
     return 0;
 }
