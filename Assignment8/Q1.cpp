c61c3934b93ac46ad2bdcd74bd3ae543573a1b96
Nihchalk <nkaur3_be24@thapar.edu>
2025-11-05 21:39:38 +0530

    Update Q1.cpp

--- Assignment8/Q1.cpp
+++ Assignment8/Q1.cpp
@@ -1,59 +1,50 @@
-#include <iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
-
-struct Node {
+class Node{
+public:
     int data;
     Node* left;
     Node* right;
-
-
-    Node(int value) {
-        data = value;
-        left = right = nullptr;
+    Node(int val){
+        data = val;
+        left = right = NULL;
     }
 };
 
-void preorder(Node* root) {
-    if (root == nullptr) return;
-    cout << root->data << " ";
+void preorder(Node* root){
+    if(!root) return;
+    cout<<root->data<<" ";
     preorder(root->left);
     preorder(root->right);
 }
 
-
-void inorder(Node* root) {
-    if (root == nullptr) return;
+void inorder(Node* root){
+    if(!root) return;
     inorder(root->left);
-    cout << root->data << " ";
+    cout<<root->data<<" ";
     inorder(root->right);
 }
 
-
-void postorder(Node* root) {
-    if (root == nullptr) return;
+void postorder(Node* root){
+    if(!root) return;
     postorder(root->left);
     postorder(root->right);
-    cout << root->data << " ";
+    cout<<root->data<<" ";
 }
 
-
-int main() {
-
+int main(){
     Node* root = new Node(1);
     root->left = new Node(2);
     root->right = new Node(3);
     root->left->left = new Node(4);
     root->left->right = new Node(5);
-    root->right->right = new Node(6);
 
-    cout << "Pre-order Traversal: ";
+    cout<<"Preorder: ";
     preorder(root);
-    cout << "\nIn-order Traversal: ";
+    cout<<"\nInorder: ";
     inorder(root);
-    cout << "\nPost-order Traversal: ";
+    cout<<"\nPostorder: ";
     postorder(root);
-
     return 0;
 }
-
